2da8bd813db413a7e343d16da15f2ffbd7dfc201
Zendo <richarduzsak@gmail.com>
2017-10-19 20:25:43 +0200

    prepared View

--- app/src/main/java/sk/stuba/fei/uamt/diplomaswork/MainActivity.java
+++ app/src/main/java/sk/stuba/fei/uamt/diplomaswork/MainActivity.java
@@ -1,15 +1,27 @@
 package sk.stuba.fei.uamt.diplomaswork;
 
+import android.app.Activity;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.support.v7.app.ActionBar;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final Handler mHandler = new Handler();
+    private Runnable mTimer1;
+    private Runnable mTimer2;
+    private LineGraphSeries<DataPoint> mSeries1;
+    //private LineGraphSeries<> mSeries2;
+    private double graph2LastXValue = 5d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +54,64 @@ public class MainActivity extends AppCompatActivity {
                 new DataPoint(4, 6)
         });
         graph.addSeries(series);
+       // graph.setTitle("EKG");
+        //graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        /*mSeries1 = new LineGraphSeries<>(generateData());
+        graph.addSeries(mSeries1);
+        graph.setTitle("EKG");*/
     }
+    /*@Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(MainActivity.ARG_SECTION_NUMBER));
+    }*/
+
+    /*@Override
+    public void onResume() {
+        super.onResume();
+        mTimer1 = new Runnable() {
+            @Override
+            public void run() {
+                mSeries1.resetData(generateData());
+                mHandler.postDelayed(this, 300);
+            }
+        };
+        mHandler.postDelayed(mTimer1, 300);
+
+        /*mTimer2 = new Runnable() {
+            @Override
+            public void run() {
+                graph2LastXValue += 1d;
+                mSeries2.appendData(new DataPoint(graph2LastXValue, getRandom()), true, 40);
+                mHandler.postDelayed(this, 200);
+            }
+        };
+        mHandler.postDelayed(mTimer2, 1000);*/
+   // }
+
+   /* @Override
+    public void onPause() {
+        mHandler.removeCallbacks(mTimer1);
+        //mHandler.removeCallbacks(mTimer2);
+        super.onPause();
+    }
+    private DataPoint[] generateData() {
+        int count = 30;
+        DataPoint[] values = new DataPoint[count];
+        for (int i=0; i<count; i++) {
+            double x = i;
+            double f = mRand.nextDouble()*0.15+0.3;
+            double y = Math.sin(i*f+2) + mRand.nextDouble()*0.3;
+            DataPoint v = new DataPoint(x, y);
+            values[i] = v;
+        }
+        return values;
+    }
+
+    double mLastRandom = 2;
+    Random mRand = new Random();
+    private double getRandom() {
+        return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
+    }*/
 }

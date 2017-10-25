d686b667a9b93795d0bb32c9c82d6cfd063e9277
Zendo <richarduzsak@gmail.com>
2017-10-25 23:57:59 +0200

    Displaying values on graph dynamically from file

--- app/src/main/java/sk/stuba/fei/uamt/diplomaswork/MainActivity.java
+++ app/src/main/java/sk/stuba/fei/uamt/diplomaswork/MainActivity.java
@@ -4,34 +4,49 @@ import android.app.Activity;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.support.v7.app.ActionBar;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
 
     private final Handler mHandler = new Handler();
     private Runnable mTimer1;
-    private Runnable mTimer2;
-    private LineGraphSeries<DataPoint> mSeries1;
-    //private LineGraphSeries<> mSeries2;
-    private double graph2LastXValue = 5d;
+    private LineGraphSeries<DataPoint> series;
+    private String graphValue;
+    private BufferedReader fileReader;
+    private DataPoint[] graphValues;
+    private int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // hideStatusBar();
         setContentView(R.layout.activity_main);
         hideStatusBar();
+        try {
+            fileReader = inicializeReader();
+            graphValue = readCsvFile(fileReader);
+            graphValues = inicializeValues(graphValue);
+            createGraph(graphValues);
+            index = 1;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void hideStatusBar(){
+    private void hideStatusBar() {
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
@@ -39,27 +54,22 @@ public class MainActivity extends AppCompatActivity {
         // Remember that you should never show the action bar if the
         // status bar is hidden, so hide that too if necessary.
         //ActionBar actionBar = getActionBar();
-        ActionBar actionBar =  getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        createGraph();
     }
 
-    private void createGraph(){
+    private void createGraph(DataPoint[] graphValues) {
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
+
+        series = new LineGraphSeries<>(graphValues);
         graph.addSeries(series);
-       // graph.setTitle("EKG");
+        // graph.setTitle("EKG");
         //graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
         /*mSeries1 = new LineGraphSeries<>(generateData());
         graph.addSeries(mSeries1);
         graph.setTitle("EKG");*/
     }
+
     /*@Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -67,51 +77,69 @@ public class MainActivity extends AppCompatActivity {
                 getArguments().getInt(MainActivity.ARG_SECTION_NUMBER));
     }*/
 
-    /*@Override
+    @Override
     public void onResume() {
         super.onResume();
         mTimer1 = new Runnable() {
             @Override
             public void run() {
-                mSeries1.resetData(generateData());
-                mHandler.postDelayed(this, 300);
+                try {
+                    if ((graphValue = readCsvFile(fileReader)) != null)
+                    {
+                        if (index > 8)
+                        {
+                            index = 0;
+                        }
+                        graphValues = updateValue(index,graphValues,graphValue);
+                        series.resetData(graphValues);
+                        index++;
+                       mHandler.postDelayed(this,500);
+                    }
+
+                } catch (FileNotFoundException e) {
+                   e.printStackTrace();
+                }
+
             }
         };
-        mHandler.postDelayed(mTimer1, 300);
+        mHandler.postDelayed(mTimer1, 500);
 
-        /*mTimer2 = new Runnable() {
-            @Override
-            public void run() {
-                graph2LastXValue += 1d;
-                mSeries2.appendData(new DataPoint(graph2LastXValue, getRandom()), true, 40);
-                mHandler.postDelayed(this, 200);
-            }
-        };
-        mHandler.postDelayed(mTimer2, 1000);*/
-   // }
+    }
 
-   /* @Override
+
+    @Override
     public void onPause() {
         mHandler.removeCallbacks(mTimer1);
-        //mHandler.removeCallbacks(mTimer2);
         super.onPause();
     }
-    private DataPoint[] generateData() {
-        int count = 30;
-        DataPoint[] values = new DataPoint[count];
-        for (int i=0; i<count; i++) {
-            double x = i;
-            double f = mRand.nextDouble()*0.15+0.3;
-            double y = Math.sin(i*f+2) + mRand.nextDouble()*0.3;
-            DataPoint v = new DataPoint(x, y);
-            values[i] = v;
+
+    private String readCsvFile(BufferedReader fileReader) throws FileNotFoundException {
+        String value = null;
+        try {
+            value = fileReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return value;
+    }
+
+    private DataPoint[] inicializeValues(String valueFromFile){
+        DataPoint[] values = new DataPoint[9];
+        values[0] = new DataPoint(0,Double.parseDouble(valueFromFile));
+        for (int i=1; i<9;i++) {
+            values[i] = new DataPoint(i,0);
+        }
+        return  values;
+    }
+
+    private DataPoint[] updateValue(int i, DataPoint[] values, String valueFromFile){
+        values[i] = new DataPoint(i,Double.parseDouble(valueFromFile));
         return values;
     }
 
-    double mLastRandom = 2;
-    Random mRand = new Random();
-    private double getRandom() {
-        return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
-    }*/
+    private BufferedReader inicializeReader(){
+        InputStream is = getResources().openRawResource(R.raw.ekgzataz);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        return reader;
+    }
 }
